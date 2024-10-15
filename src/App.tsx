5d891d1df135368d0d3ec849cd90f70a19342731
RatLad69 <quirinus613@gmail.com>
2024-10-15 00:49:49 -0500

    table style

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,23 @@ import "./App.css";
 
 function NumSquare({ value, location }: { value: number; location: number }) {
 	return (
-		<div className="num-square" onClick={() => console.log(location)}>
+		<div
+			className="num-square"
+			onClick={() => console.log(location + ": " + value)}
+		>
 			{value.toString()}
 		</div>
 	);
 }
 
-function SmallGrid({ values }: { values: number[] }) {
-	// return values.map((value, i) => (
-	// 	<NumSquare value={value} key={i}></NumSquare>
-	// ));
+function SmallGrid({ values, cols }: { values: number[]; cols: number }) {
 	let rows = [];
-	const numsPerRow = 3;
+	// const numsPerRow = 3;
 	let row = [];
-	for (let i = 0; i < Math.floor(values.length / numsPerRow); i++) {
+	for (let i = 0; i < Math.floor(values.length / cols); i++) {
 		row = [];
-		for (let j = 0; j < numsPerRow; j++) {
-			row.push(values[numsPerRow * i + j]);
+		for (let j = 0; j < cols; j++) {
+			row.push(values[cols * i + j]);
 		}
 		rows.push(row);
 	}
@@ -54,7 +54,7 @@ function App() {
 
 	return (
 		<>
-			<SmallGrid values={testVals}></SmallGrid>
+			<SmallGrid values={testVals} cols={3}></SmallGrid>
 			<div>{testTxt}</div>
 		</>
 	);
