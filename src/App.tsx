f441ea929effe60f6ca62c2951e6fa90299b7319
RatLad69 <quirinus613@gmail.com>
2024-10-28 01:42:45 -0500

    entering numbers updates state

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,45 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
-function NumSquare({ value, location }: { value: number; location: number }) {
+function NumSquare({
+	value,
+	location,
+	updateNum,
+}: {
+	value: number;
+	location: number;
+	updateNum: (idx: number, nextVal: number) => void;
+}) {
+	function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+		updateNum(location, parseFloat(e.target.value));
+	}
+
 	return (
 		<div
 			className="num-square"
-			onClick={() => console.log(location + ": " + value)}
+			// onClick={() => console.log(location + ": " + value)}
 		>
-			{value.toString()}
+			{/* {value.toString()} */}
+			<input
+				type="number"
+				value={value}
+				max={9}
+				min={1}
+				onChange={handleChange}
+			></input>
 		</div>
 	);
 }
 
-function NumGrid({ values, cols }: { values: number[]; cols: number }) {
+function NumGrid({
+	values,
+	cols,
+	updateNum,
+}: {
+	values: number[];
+	cols: number;
+	updateNum: (idx: number, nextVal: number) => void;
+}) {
 	let rows = [];
 	let row = [];
 	for (let i = 0; i < Math.floor(values.length / cols); i++) {
@@ -32,8 +59,11 @@ function NumGrid({ values, cols }: { values: number[]; cols: number }) {
 							<td key={j}>
 								<NumSquare
 									value={value}
-									location={10 * (i + 1) + j + 1}
-									key={10 * (i + 1) + j + 1} // First digit of key is row; second is column
+									location={cols * i + j}
+									key={cols * i + j} // key/location == index of value in values array
+									// location={10 * (i + 1) + j + 1}
+									// key={10 * (i + 1) + j + 1} // First digit of key is row; second is column
+									updateNum={updateNum}
 								></NumSquare>
 							</td>
 						))}
@@ -47,6 +77,18 @@ function NumGrid({ values, cols }: { values: number[]; cols: number }) {
 function App() {
 	let [testVals, setTestVals] = useState([1, 2, 3]);
 	let [testTxt, setTestTxt] = useState("banana");
+
+	function updateNum(idx: number, nextVal: number) {
+		const nextVals = testVals.map((val, i) => {
+			if (i === idx) {
+				return nextVal;
+			} else {
+				return val;
+			}
+		});
+		setTestVals(nextVals);
+	}
+
 	useEffect(() => {
 		setTestVals([
 			2, 3, 4, 5, 6, 7, 8, 9, 1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5,
@@ -73,7 +115,7 @@ function App() {
 
 	return (
 		<>
-			<NumGrid values={testVals} cols={9}></NumGrid>
+			<NumGrid values={testVals} cols={9} updateNum={updateNum}></NumGrid>
 			<div>{testVals}</div>
 			<div>{testTxt}</div>
 		</>
